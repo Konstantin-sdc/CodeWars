68c7b3898f4f93b7cb9359143f2663e694520332
Konstantin-sdc <konstantinsdc@gmail.com>
2018-07-09 13:39:58 +0300

    Добавлен проект для тестирования.

---
+++ UnitTestProject1/CodeWarsTestProject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeWarsTestProject {
+  [TestClass]
+  public class CodeWarsTest {
+    [TestMethod]
+    public void TestMethod1() {
+    }
+  }
+}
