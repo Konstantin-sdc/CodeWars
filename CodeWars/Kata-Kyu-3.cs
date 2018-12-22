92111f8a1a57b6a06c2673c683ef7f4b8c3f0673
Konstantin-sdc <konstantinsdc@gmail.com>
2018-12-22 14:39:59 +0300

    Шаблон ката     [KataType(LevelTypeEnum.Kyu, 3, "589394ae1a880832e2000092")] DefineClass — создание класса во время выпонения

---
+++ CodeWars/Kata-Kyu-3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeWars {
+  public partial class Kata {
+
+    /// <summary>Интерактивное создание класса</summary>
+    /// <param name="className">Имя класса</param>
+    /// <param name="properties">Свойства класса (имя, тип)</param>
+    /// <param name="actualType"></param>
+    /// <returns>false, если класс уже существует в этой сборке</returns>
+    [KataType(LevelTypeEnum.Kyu, 3, "589394ae1a880832e2000092")]
+    public static bool DefineClass(string className, Dictionary<string, Type> properties, ref Type actualType) {
+      return true;
+    }
+
+  }
+
+}
+
--- UnitTestProject1/KataTests.cs
+++ UnitTestProject1/KataTests.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using CodeWars;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+
+using System.Reflection;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CodeWars.Tests {
+  public class SomeClass {
+  }
 
   [TestClass()]
   public class KataTests {
@@ -292,6 +297,42 @@ namespace CodeWars.Tests {
       IEnumerable<string> q = Kata.GetDep(dic, "g");
       //var w = Kata.ExpandDependencies(dic);
     }
+
+    [TestMethod()]
+    public void DefineClassTest() {
+      var rand = new Random();
+      Type myType = typeof(object);
+      Dictionary<string, Type> properties;
+
+      // Define first class
+      properties = new Dictionary<string, Type> {
+        { "SomeInt", typeof(int) },
+        { "SomeString", typeof(string) },
+        { "SomeObject", typeof(object) }
+      };
+      Kata.DefineClass("SomeClass", properties, ref myType);
+      // Instantiate first class
+      var myInstance = Activator.CreateInstance(myType);
+      myInstance.SomeObject = myInstance;
+      myInstance.SomeString = "Hey there";
+      myInstance.SomeInt = 3;
+      Console.WriteLine($"{myInstance.SomeObject}: {myInstance.SomeString}, {myInstance.SomeInt}");
+
+      // Define second class
+      properties = new Dictionary<string, Type> { { "AnotherObject", typeof(object) }, { "SomeDouble", typeof(double) }, { "AnotherString", typeof(string) } };
+      Kata.DefineClass("AnotherClass_N" + rand.Next(100), properties, ref myType);
+      // Instantiate second class
+      myInstance = CreateInstance(myType);
+      myInstance.AnotherObject = "User: ";
+      myInstance.AnotherString = "My lucky number is ";
+      myInstance.SomeDouble = 92835768;
+      Console.WriteLine($"{myInstance.AnotherObject}: {myInstance.AnotherString} {myInstance.SomeDouble} ");
+
+      // Try to redefine first class
+      if(Kata.DefineClass("SomeClass", properties, ref myType) == true)
+        Assert.Fail("This class is already defined");
+    }
+
   }
 
 }
