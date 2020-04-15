6b478ee83b13f37cb1bf07ca8f35ee75257ea6fd
Константин Бутенин <konstantinsdc@gmail.com>
2020-04-15 17:02:12 +0300

    Перетасовка

--- CodeWars/Kata/Kyu/L6/Kata-Kyu-6.cs
+++ CodeWars/Kata/Kyu/L6/Kata-Kyu-6.cs
@@ -6,23 +6,6 @@ namespace CodeWars {
 
     public static partial class KataClass {
 
-        /// <summary>Вернуть первую наидлиннейшую строку, состоящую из k элементов массива</summary>
-        /// <param name="strarr">Входящий массив</param>
-        /// <param name="k">Нужное число элементов</param>
-        /// <returns>Строка</returns>
-        [KataType(LevelTypeEnum.Kyu, 6)]
-        public static String LongestConsec(string[] strarr, int k) {
-            if(k > strarr.Length) return String.Empty;
-            // Скользящая группировка
-            string result = String.Empty;
-            for(int i = 0; i < strarr.Length; i++) {
-                IEnumerable<string> subGroup = strarr.Skip(i).Take(k);
-                string subString = String.Join(String.Empty, subGroup);
-                if(subString.Length > result.Length) result = subString;
-            }
-            return result;
-        }
-
         /// <summary>Возвращает массив чисел из входящего диапазона с такими свойствами, что эти числа равны сумме составляющих их цифр, 
         /// в степенях, равных местам цифр в числе. (например: 135 = 1^1 + 3^2 + 5^3)
         /// </summary>
---
+++ CodeWars/Kata/Kyu/L6/LongestConsecutives.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars.Kata.Kyu.L6 {
+
+    class LongestConsecutives {
+
+        /// <summary>Вернуть первую наидлиннейшую строку, состоящую из k элементов массива</summary>
+        /// <param name="strarr">Входящий массив</param>
+        /// <param name="k">Нужное число элементов</param>
+        /// <returns>Строка</returns>
+        [KataType(LevelTypeEnum.Kyu, 6)]
+        public static String LongestConsec(string[] strarr, int k) {
+            if (k > strarr.Length) return String.Empty;
+            // Скользящая группировка
+            string result = String.Empty;
+            for (int i = 0; i < strarr.Length; i++) {
+                IEnumerable<string> subGroup = strarr.Skip(i).Take(k);
+                string subString = String.Join(String.Empty, subGroup);
+                if (subString.Length > result.Length) result = subString;
+            }
+            return result;
+        }
+
+    }
+
+}
