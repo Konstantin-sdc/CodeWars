3f6cc253986b285857d040c0b8d21a1a6d48895f
Константин Бутенин <konstantinsdc@gmail.com>
2020-04-15 12:37:31 +0300

    Перетасовка

--- CodeWars/Kata/Kyu/L5/Kata-Kyu-5.cs
+++ CodeWars/Kata/Kyu/L5/Kata-Kyu-5.cs
@@ -54,40 +54,6 @@ namespace CodeWars {
             return fibSum * 4;
         }
 
-        /// <summary>
-        /// Возвращает возможность объединения и перемешиваения двух строку в третию, 
-        /// без изменения порядка символов в исходных строках.
-        /// </summary>
-        /// <param name="s">Итоговая строка</param>
-        /// <param name="part1">Первая строка</param>
-        /// <param name="part2">Вторая строка</param>
-        /// <returns>true, если объединение возможно</returns>
-        [KataType(LevelTypeEnum.Kyu, 5)]
-        public static bool IsMerge(string s, string part1, string part2) {
-            return MayExtract(s, part1, part2);
-            //NOTE Символы в составляющих строках могут повторятся, совпадать с исмволами другой строки, быть разного регистра.
-        }
-
-        private static bool MayExtract(string s, params string[] parts) {
-            string ordS = string.Join("", s.OrderBy(k => k));
-            string p = string.Join("", parts);
-            string ordP = string.Join("", p.OrderBy(k => k));
-            if(!ordS.Equals(ordP)) {
-                return false;
-            }
-
-            foreach(string part in parts) {
-                int start = 0;
-                foreach(char item in part) {
-                    start = s.IndexOf(item, start);
-                    if(start == -1) {
-                        return false;
-                    }
-                }
-            }
-            return true;
-        }
-
         /// <summary>Интерпретатор</summary>
         /// <param name="code">Код Paintfuck, который должен быть выполнен, передается как строка. </param>
         /// <param name="iterations"></param>
