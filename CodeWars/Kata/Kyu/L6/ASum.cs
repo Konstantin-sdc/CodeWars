834452c752eb1532abafc13f86d1fc6fbd90aa1a
Константин Бутенин <konstantinsdc@gmail.com>
2020-04-15 12:33:10 +0300

    Перетасовка

---
+++ CodeWars/Kata/Kyu/L6/ASum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeWars.Kata.Kyu.L6 {
+
+    public class ASum {
+
+        /// <summary>
+        /// <para>Возвращает количество кубов, нужное для достижения объёма = m.</para>
+        /// <para>Кубы начинаются с размеров 1х1х1.</para>
+        /// <para>Сторона каждого последующего куба больше стороны предыдущего на 1.</para>
+        /// </summary>
+        /// <param name="m">Нужный объём</param>
+        /// <returns>Количество кубов или -1, если объём не может уместить количество.</returns>
+        [KataType(LevelTypeEnum.Kyu, 6)]
+        public static long FindNb(params long[] m) {
+            long s = 1, v = 0, count = 0;
+            for (; v < m[0]; s++, count++) {
+                v += s * s * s;
+            }
+            return (m[0] == v) ? count : -1;
+        }
+
+    }
+
+}
--- CodeWars/Kata/Kyu/L6/Kata-Kyu-6.cs
+++ CodeWars/Kata/Kyu/L6/Kata-Kyu-6.cs
@@ -91,22 +91,6 @@ namespace CodeWars {
             catch { return int.MaxValue; }
         }
 
-        /// <summary>
-        /// <para>Возвращает количество кубов, нужное для достижения объёма = m.</para>
-        /// <para>Кубы начинаются с размеров 1х1х1.</para>
-        /// <para>Сторона каждого последующего куба больше стороны предыдущего на 1.</para>
-        /// </summary>
-        /// <param name="m">Нужный объём</param>
-        /// <returns>Количество кубов или -1, если объём не может уместить количество.</returns>
-        [KataType(LevelTypeEnum.Kyu, 6)]
-        public static long FindNb(params long[] m) {
-            long s = 1, v = 0, count = 0;
-            for(; v < m[0]; s++, count++) {
-                v += s * s * s;
-            }
-            return (m[0] == v) ? count : -1;
-        }
-
     }
 
 }
