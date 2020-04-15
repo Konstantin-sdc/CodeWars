394aebbab7c7f54a2e62f75254dba6a8fc9a666c
Константин Бутенин <konstantinsdc@gmail.com>
2020-04-15 16:57:22 +0300

    Перетасовка

--- CodeWars/Kata/Kyu/L6/Kata-Kyu-6.cs
+++ CodeWars/Kata/Kyu/L6/Kata-Kyu-6.cs
@@ -6,24 +6,6 @@ namespace CodeWars {
 
     public static partial class KataClass {
 
-        /// <summary>Возвращает количество операций умножения, которые нужно провести над цифрами данного числа до тех пор, 
-        /// пока они не станут одной цифрой.</summary>
-        /// <param name="n">Проверяемое число</param>
-        /// <returns>Необходимое число умножений</returns>
-        [KataType(LevelTypeEnum.Kyu, 6)]
-        public static int Persistence(long n) {
-            if(n < 10) return 0;
-            IEnumerable<double> digits = n.ToString().Select(e => Char.GetNumericValue(e));
-            long baseNumber = 1;
-            foreach(long digit in digits) {
-                baseNumber *= digit;
-                if(digit == 0) break;
-            }
-            int multCount = Persistence(baseNumber);
-            multCount++;
-            return multCount;
-        }
-
         /// <summary>Вернуть первую наидлиннейшую строку, состоящую из k элементов массива</summary>
         /// <param name="strarr">Входящий массив</param>
         /// <param name="k">Нужное число элементов</param>
