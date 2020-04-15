ca63981f8f084df3332f61159795e386dcf1ed3b
Константин Бутенин <konstantinsdc@gmail.com>
2020-04-15 16:15:08 +0300

    Перетасовка

--- CodeWars/Kata/Kyu/L5/Kata-Kyu-5.cs
+++ CodeWars/Kata/Kyu/L5/Kata-Kyu-5.cs
@@ -10,35 +10,6 @@ namespace CodeWars {
 
     public static partial class KataClass {
 
-        /// <summary>Возвращает два числа из натурального ряда, произведение которых равно сумме всех чисел этого ряда, исключая эти два числа</summary>
-        /// <param name="n">Верхний предел ряда. Всегда > 0</param>
-        /// <returns>Массив чисел</returns>
-        [KataType(LevelTypeEnum.Kyu, 5)]
-        public static List<long[]> RemovNb(long n) {
-            List<long[]> result = new List<long[]>();
-            // Вычислить сумму натурального ряда от 1 до n
-            long sum;
-            checked {
-                sum = (n + 1) * n / 2;
-            }
-            // Минимальный первый элемент
-            long firstMin = (sum - 2 * n + 1) / n;
-            // Максимальный первый элемент
-            long firstMax = (sum - 2 * firstMin - 1) / firstMin;
-            // Второй элемент
-            long searchRange = firstMax - firstMin;
-            for(long i = firstMin; i <= firstMax; i++) {
-                long a = sum - i;
-                long b = i + 1;
-                if(a % b > 0) {
-                    continue;
-                }
-
-                result.Add(new long[2] { i, a / b });
-            }
-            return result;
-        }
-
         /// <summary>Интерпретатор</summary>
         /// <param name="code">Код Paintfuck, который должен быть выполнен, передается как строка. </param>
         /// <param name="iterations"></param>
---
+++ CodeWars/Kata/Kyu/L5/RemovedNumbers.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace CodeWars.Kata.Kyu.L5 {
+
+    class RemovedNumbers {
+
+        /// <summary>
+        /// Возвращает два числа из натурального ряда, произведение которых равно сумме всех чисел этого ряда, исключая эти два числа.
+        /// </summary>
+        /// <param name="n">Верхний предел ряда. Всегда > 0.</param>
+        /// <returns>Массив чисел.</returns>
+        [KataType(LevelTypeEnum.Kyu, 5)]
+        public static List<long[]> RemovNb(long n) {
+            List<long[]> result = new List<long[]>();
+            // Вычислить сумму натурального ряда от 1 до n
+            long sum;
+            checked {
+                sum = (n + 1) * n / 2;
+            }
+            // Минимальный первый элемент
+            long firstMin = (sum - 2 * n + 1) / n;
+            // Максимальный первый элемент
+            long firstMax = (sum - 2 * firstMin - 1) / firstMin;
+            // Второй элемент
+            long searchRange = firstMax - firstMin;
+            for (long i = firstMin; i <= firstMax; i++) {
+                long a = sum - i;
+                long b = i + 1;
+                if (a % b > 0) {
+                    continue;
+                }
+
+                result.Add(new long[2] { i, a / b });
+            }
+            return result;
+        }
+
+    }
+
+}
