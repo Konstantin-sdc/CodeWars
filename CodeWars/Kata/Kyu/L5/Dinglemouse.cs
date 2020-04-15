fe90242ce27438c9519fa3ea0e007da14b8a283a
Константин Бутенин <konstantinsdc@gmail.com>
2020-04-15 12:25:27 +0300

    Перетасовка

---
+++ CodeWars/Kata/Kyu/L5/Dinglemouse.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace CodeWars.Kata.Kyu.L5 {
+
+    class Dinglemouse {
+
+        private static readonly Dictionary<string, string> _soundDict = new Dictionary<string, string>() {
+            { "(?!^)[HW]", "" },
+            { "[BFPV]", "1" },
+            { "[CGJKQSXZ]", "2" },
+            { "[DT]", "3" },
+            { "[L]", "4" },
+            { "[MN]", "5" },
+            { "[R]", "6" },
+            { @"(\d+)\1", "$1"},
+            { @"(?!^)[AEIOUY]", "" },
+            { @"$", "000" }
+        };
+
+        /// <summary>Преобразует слова английского языка по алгоритму SOUNDEX</summary>
+        /// <param name="names">Строка из слов, разделённых пробелом</param>
+        /// <returns>SOUNDEX-коды группы слов</returns>
+        [KataType(LevelTypeEnum.Kyu, 5)]
+        public static string Soundex(string names) {
+            string[] result = names.Split(' ');
+            for (int i = 0; i < result.Length; i++) {
+                result[i] = SoundexSingle(result[i]);
+            }
+            return string.Join(" ", result);
+        }
+
+        private static string SoundexSingle(string word) {
+            string result = word.ToUpper();
+            char fC = result[0];
+            foreach (KeyValuePair<string, string> item in _soundDict) {
+                result = Regex.Replace(result, item.Key, item.Value);
+            }
+            result = fC + result.Substring(1);
+            return result.Substring(0, 4);
+        }
+
+    }
+
+}
--- CodeWars/Kata/Kyu/L5/Kata-Kyu-5.cs
+++ CodeWars/Kata/Kyu/L5/Kata-Kyu-5.cs
@@ -110,41 +110,6 @@ namespace CodeWars {
             return "";
         }
 
-        private static Dictionary<string, string> _soundDict = new Dictionary<string, string>() {
-      { "(?!^)[HW]", "" },
-      { "[BFPV]", "1" },
-      { "[CGJKQSXZ]", "2" },
-      { "[DT]", "3" },
-      { "[L]", "4" },
-      { "[MN]", "5" },
-      { "[R]", "6" },
-      { @"(\d+)\1", "$1"},
-      { @"(?!^)[AEIOUY]", "" },
-      { @"$", "000" }
-    };
-
-        /// <summary>Преобразует слова английского языка по алгоритму SOUNDEX</summary>
-        /// <param name="names">Строка из слов, разделённых пробелом</param>
-        /// <returns>SOUNDEX-коды группы слов</returns>
-        [KataType(LevelTypeEnum.Kyu, 5)]
-        public static string Soundex(string names) {
-            string[] result = names.Split(' ');
-            for(int i = 0; i < result.Length; i++) {
-                result[i] = SoundexSingle(result[i]);
-            }
-            return string.Join(" ", result);
-        }
-
-        private static string SoundexSingle(string word) {
-            string result = word.ToUpper();
-            char fC = result[0];
-            foreach(KeyValuePair<string, string> item in _soundDict) {
-                result = Regex.Replace(result, item.Key, item.Value);
-            }
-            result = fC + result.Substring(1);
-            return result.Substring(0, 4);
-        }
-
         /// <summary>Кодовая строка</summary>
         private static string _codeString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
