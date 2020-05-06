e8c3c507d1f51632553f7c0023e9e195f0709f4d
Константин Бутенин <konstantinsdc@gmail.com>
2020-05-06 09:03:30 +0300

    SmallFuck completed.

--- CodeWars/Kata/Kyu/L5/SmallFuck.cs
+++ CodeWars/Kata/Kyu/L5/SmallFuck.cs
@@ -26,17 +26,16 @@
             // < -Move pointer to the left(by 1 cell)
             // * - Flip the bit at the current cell
             // [ - Jump past matching ] if value at current cell is 0
-            // ] - Jump back to matching [(if value at current cell is nonzero)
+            // ] - Jump back to matching [ if value at current cell is nonzero
             #endregion
-            var paireds = GetPairedPosition(code, '[', ']');
-            var data = source.Where(e => e == 0 || e == 1).ToList();
+            var paireds = GetPairedPositions(code, '[', ']');
+            var data = source.Where(e => e == '0' || e == '1').ToList();
             var dataPoint = 0;
             for (var i = 0; i < code.Length; i++)
             {
                 if (dataPoint < 0 || dataPoint >= data.Count)
                     break;
                 var command = code[i];
-
                 switch (command)
                 {
                     case '>':
@@ -45,8 +44,7 @@
                     case '<':
                         --dataPoint;
                         continue;
-                    case '*':
-                        data.
+                    case '*':                        
                         data[dataPoint] = data[dataPoint] == '0' ? '1' : '0';
                         continue;
                     case '[':
@@ -62,11 +60,19 @@
             return string.Concat(data);
         }
 
+        /// <summary>Смещает указатель кода вперёд на парную позицию.</summary>
+        /// <param name="codePount">Текущий указатель кода.</param>
+        /// <param name="currentData">Текущее значение данных под указателем.</param>
+        /// <param name="paireds">Словарь расположения пар.</param>
         private static void CodeForward(ref int codePount, char currentData, Dictionary<int, int> paireds)
         {
             if (currentData == '0') codePount = paireds[codePount];
         }
 
+        /// <summary>Смещает указатель кода назад на парную позицию.</summary>
+        /// <param name="codePount">Текущий указатель кода.</param>
+        /// <param name="currentData">Текущее значение данных под указателем.</param>
+        /// <param name="paireds">Словарь расположения пар.</param>
         private static void CodeBack(ref int codePount, char currentData, Dictionary<int, int> paireds)
         {
             if (currentData == '1')
@@ -82,7 +88,7 @@
         /// <param name="open">Элемент первый в паре</param>
         /// <param name="close">Элемент второй в паре</param>
         /// <returns>Словарь</returns>
-        private static Dictionary<int, int> GetPairedPosition<T>(IEnumerable<T> code, T open, T close)
+        private static Dictionary<int, int> GetPairedPositions<T>(IEnumerable<T> code, T open, T close)
         {
             if (code.Count() < 2 || !code.Contains(open)) return null;
             var result = new Dictionary<int, int>();
@@ -94,7 +100,6 @@
                 if (code.ElementAt(i).Equals(open))
                     opPositions.Add(i);
             }
-
             foreach (var position in opPositions)
             {
                 for (var i = position; i < code.Count(); i++)
@@ -108,7 +113,6 @@
                         closCount++;
                     else
                         continue;
-
                     if (opCount != 0 && opCount == closCount)
                     {
                         result.Add(position, i);
@@ -118,7 +122,6 @@
                     }
                 }
             }
-
             return result;
         }
 
---
+++ CodeWarsTests/Kata/Kyu/L5/SmallFuckTests.cs
@@ -0,0 +1,30 @@
+﻿namespace CodeWars.Kata.Kyu.Esolangs.Tests
+{
+    using System.Collections.Generic;
+
+    using Xunit;
+
+    public class SmallFuckTests
+    {
+        private static readonly Dictionary<(string code, string source), string> _testValues = new Dictionary<(string, string), string>()
+        {
+            {("*", "00101100"), "10101100" },
+            {(">*>*", "00101100"), "01001100" },
+            {("*>*>*>*>*>*>*>*", "00101100"), "11010011" },
+            {("*>*>>*>>>*>*", "00101100"), "11111111" },
+            {(">>>>>*<*<<*", "00101100"), "00000000" },
+        };
+
+
+        [Fact()]
+        public void InterpreterTest()
+        {
+            foreach (var item in _testValues)
+            {
+                var cd = item.Key.code;
+                var src = item.Key.source;
+                Assert.True(SmallFuck.Interpreter(cd, src) == item.Value);
+            }
+        }
+    }
+}
